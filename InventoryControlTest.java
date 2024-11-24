0669b54b41ba9b757a056b892e614afff5c94552
Paulina Lujan <paulina2801@icloud.com>
2024-11-23 22:07:22 -0700

    Added Test file for inventory, needs to be tracked in order to test.

---
+++ InventoryControlTest.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class InventoryControlTest {
+
+    public static void main(String[] args){
+        // ArrayList<Inventory> testInventory = new ArrayList<Inventory>();
+        System.out.println("HERE");
+    }   
+
+}
